00d252d15b7dd9c407b81a7761dfeb06cce55935
nnk-ymmt <garden.n1234567890@gmail.com>
2020-12-02 23:53:45 +0900

    Initial commit

---
+++ Part8-commonValueSlider-/SecondViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SecondViewController.swift
+//  Part8-commonValueSlider-
+//
+//  Created by 山本ののか on 2020/12/02.
+//
+
+import UIKit
+
+final class SecondViewController: UIViewController {
+
+    @IBOutlet private weak var valueLabel: UILabel!
+    @IBOutlet private weak var slider: UISlider!
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        if let sliderValue = UserDefaults.standard.object(forKey: "sliderValue") as? Float {
+            valueLabel.text = String(sliderValue)
+            slider.value = sliderValue
+        }
+    }
+
+    @IBAction private func changeSliderValue(_ sender: UISlider) {
+        valueLabel.text = String(sender.value)
+        UserDefaults.standard.setValue(sender.value, forKey: "sliderValue")
+    }
+}
