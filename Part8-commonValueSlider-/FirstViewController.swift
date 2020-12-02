3eb8dafc6456b37354de3d3bf04dbf8a72127b4a
nnk-ymmt <garden.n1234567890@gmail.com>
2020-12-03 00:16:13 +0900

    appDelegateを使用したやり方追加

--- Part8-commonValueSlider-/FirstViewController.swift
+++ Part8-commonValueSlider-/FirstViewController.swift
@@ -12,16 +12,27 @@ final class FirstViewController: UIViewController {
     @IBOutlet private weak var valueLabel: UILabel!
     @IBOutlet private weak var slider: UISlider!
 
+    let delegate = UIApplication.shared.delegate as! AppDelegate
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
-        if let sliderValue = UserDefaults.standard.object(forKey: "sliderValue") as? Float {
-            valueLabel.text = String(sliderValue)
-            slider.value = sliderValue
-        }
+//        if let sliderValue = UserDefaults.standard.object(forKey: "sliderValue") as? Float {
+//            valueLabel.text = String(sliderValue)
+//            slider.value = sliderValue
+//        }
+        valueLabel.text = String(delegate.sliderValue)
+        slider.value = delegate.sliderValue
     }
 
     @IBAction private func changeSliderValue(_ sender: UISlider) {
         valueLabel.text = String(sender.value)
-        UserDefaults.standard.setValue(sender.value, forKey: "sliderValue")
+//        UserDefaults.standard.setValue(sender.value, forKey: "sliderValue")
+        delegate.sliderValue = sender.value
     }
 }
+
+
+
+// 解答例
+
+// UserDefaultsを使用したやり方はコメントアウト
--- Part8-commonValueSlider-/SecondViewController.swift
+++ Part8-commonValueSlider-/SecondViewController.swift
@@ -12,16 +12,21 @@ final class SecondViewController: UIViewController {
     @IBOutlet private weak var valueLabel: UILabel!
     @IBOutlet private weak var slider: UISlider!
 
+    let delegate = UIApplication.shared.delegate as! AppDelegate
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
-        if let sliderValue = UserDefaults.standard.object(forKey: "sliderValue") as? Float {
-            valueLabel.text = String(sliderValue)
-            slider.value = sliderValue
-        }
+//        if let sliderValue = UserDefaults.standard.object(forKey: "sliderValue") as? Float {
+//            valueLabel.text = String(sliderValue)
+//            slider.value = sliderValue
+//        }
+        valueLabel.text = String(delegate.sliderValue)
+        slider.value = delegate.sliderValue
     }
 
     @IBAction private func changeSliderValue(_ sender: UISlider) {
         valueLabel.text = String(sender.value)
-        UserDefaults.standard.setValue(sender.value, forKey: "sliderValue")
+//        UserDefaults.standard.setValue(sender.value, forKey: "sliderValue")
+        delegate.sliderValue = sender.value
     }
 }
